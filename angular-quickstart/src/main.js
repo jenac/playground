791ab50b1ea9396136ce7943d02f80f4b7038eb9
Lihe Chen <lihe.chen@gmail.com>
2017-03-23 19:38:17 -0500

    correction, it works!

--- angular-quickstart/src/main.js
+++ angular-quickstart/src/main.js
@@ -1,7 +1,7 @@
-(function(app){
-  document.addEventListener('DOMContentLoaded', function(){
+(function(app) {
+  document.addEventListener('DOMContentLoaded', function() {
     ng.platformBrowserDynamic
       .platformBrowserDynamic()
-      .bootstrapModule(app.Module);
+      .bootstrapModule(app.AppModule);
   });
 })(window.app || (window.app = {}));
