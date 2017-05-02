89adda7e0e7b4b757a6451a31ca3a6705c6866d4
Lihe Chen <lihe.chen@gmail.com>
2017-05-01 20:06:00 -0700

    added for something

---
+++ Ex001/Ex001/Data.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ex001
+{
+    public class Identity
+    {
+        public string User { get; set; }
+        public string Pass { get; set; }
+        public string ServicePanel { get; set; }
+    }
+
+    public class Version {
+        public string MessageVersion { get; set; }
+        public string MessageRevision { get; set; }
+    }
+    public class EnterpriseHeader
+    {
+        public Identity Identity { get; set; }
+        public Version Version { get; set; }
+    }
+
+    public class Credential
+    {
+        public EnterpriseHeader EnterpriseHeader { get; set; }
+        public String IdNumber { get; set; }
+        public String Pin { get; set; }
+    }
+
+
+    public class Authentication {
+        public Credential Input { get; set; }
+        public DateTime Authenticated { get; set; }
+        public Boolean Ok { get; set; }
+    }
+}
