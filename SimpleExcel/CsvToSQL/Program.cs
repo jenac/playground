85462a190da407ff2641958837be389895010d38
Lihe Chen <lchen@tcfbank.com>
2017-06-13 14:34:36 -0500

    simple excel

---
+++ SimpleExcel/CsvToSQL/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvToSQL
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<String> sqls = new List<string>();
+            var files = Directory.GetFiles(@"c:\csvs", "*.csv");
+            foreach(var file in files)
+            {
+                string title = Path.GetFileNameWithoutExtension(file);
+                Console.WriteLine(title);
+                var lines = File.ReadAllLines(file);
+                if (lines[0] != "Issued To,Issued By,Expiration Date,Intended Purposes,Friendly Name,Status,Certificate Template")
+                {
+                    continue;
+                }
+                foreach (string line in lines)
+                {
+                    if (line == "Issued To,Issued By,Expiration Date,Intended Purposes,Friendly Name,Status,Certificate Template")
+                    {
+                        continue;
+                    }
+                    var sa = line.Trim().Split(new char[] { ','});
+                    if (sa.Length == 8)
+                    {
+                        string sqlfmt = @"INSERT INTO [OtherCerts]
+           ([Category]
+           ,[IssuedTo]
+           ,[IssuedBy]
+           ,[ExpirationDate]
+           ,[IntendedPurposes]
+           ,[FriendlyName]
+           ,[Status]
+           ,[CertificateTemplate])
+     VALUES
+           ('{0}'
+           ,'{1}'
+           ,'{2}'
+           ,'{3}'
+           ,'{4}'
+           ,'{5}'
+           ,'{6}'
+           ,'{7}');";
+                        DateTime p = DateTime.Parse(sa[2]);
+                        string sql = string.Format(sqlfmt, title, sa[0], sa[1], p.ToString("yyyy-MM-dd"), sa[3], sa[4], sa[5], sa[6]);
+                        sqls.Add(sql);
+                    }
+                }
+                
+                
+            }
+            File.WriteAllLines(@"C:\csvs\x.sql", sqls.ToArray());
+        }
+    }
+}
---
+++ SimpleExcel/SimpleExcel/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace SimpleExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\2.xlsx");
+            //Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+
+            foreach(Excel._Worksheet xlWorksheet in xlWorkbook.Sheets)
+            {
+                String name = xlWorksheet.Name;
+                xlWorksheet.SaveAs(Filename: $"c:\\csvs\\{name}.csv", FileFormat: Excel.XlFileFormat.xlCSV);
+                Excel.Range xlRange = xlWorksheet.UsedRange;
+                Console.WriteLine(name);
+                //cleanup
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
+                //rule of thumb for releasing com objects:
+                //  never use two dots, all COM objects must be referenced and released individually
+                //  ex: [somthing].[something].[something] is bad
+
+                //release com objects to fully kill excel process from running in the background
+                Marshal.ReleaseComObject(xlRange);
+                Marshal.ReleaseComObject(xlWorksheet);
+            }
+            
+            
+
+
+
+            
+
+            //close and release
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //quit and release
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+        }
+    }
+}
