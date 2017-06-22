e935d80f99f092da998f598693ba76905f4dd7e1
Lihe Chen <lihe.chen@gmail.com>
2017-06-22 16:25:36 -0500

    init saml-poc

---
+++ saml-poc/src/main/java/com/playground/saml/samlpoc/SamlPocApplication.java
@@ -0,0 +1,12 @@
+package com.playground.saml.samlpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamlPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamlPocApplication.class, args);
+	}
+}
---
+++ saml-poc/src/test/java/com/playground/saml/samlpoc/SamlPocApplicationTests.java
@@ -0,0 +1,16 @@
+package com.playground.saml.samlpoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SamlPocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
