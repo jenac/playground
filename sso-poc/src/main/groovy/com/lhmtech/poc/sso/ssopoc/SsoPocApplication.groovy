0fbca1c1778903f42df4ad2ac45f24ab1ef7d7b1
Lihe Chen <lihe.chen@gmail.com>
2017-08-22 11:37:08 -0500

    wip

---
+++ sso-poc/src/main/groovy/com/lhmtech/poc/sso/ssopoc/SsoPocApplication.groovy
@@ -0,0 +1,12 @@
+package com.lhmtech.poc.sso.ssopoc
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SsoPocApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run SsoPocApplication, args
+	}
+}
