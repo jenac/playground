8ebcf0919a98bc97065e6fba7eb7e154d8b2cb46
Lihe Chen <lihe.chen@changehealthcare.com>
2017-09-26 14:31:00 -0500

    create project

---
+++ pdf-poc/src/main/groovy/com/example/pdfpoc/PdfPocApplication.groovy
@@ -0,0 +1,12 @@
+package com.example.pdfpoc
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PdfPocApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run PdfPocApplication, args
+	}
+}
