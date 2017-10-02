6c4db0d30e9c0ae9e36c1eb8e250e01a5a40d686
Lihe Chen <lihe.chen@changehealthcare.com>
2017-10-02 09:00:50 -0500

    WIP

---
+++ pdf-poc/src/main/groovy/com/example/pdfpoc/services/PdfMaker.groovy
@@ -0,0 +1,7 @@
+package com.example.pdfpoc.services
+
+/**
+ * Created by lichen on 9/27/17.
+ */
+class PdfMaker {
+}
---
+++ pdf-poc/src/test/groovy/com/example/pdfpoc/services/PdfMakerSpec.groovy
@@ -0,0 +1,9 @@
+package com.example.pdfpoc.services
+
+/**
+ * Created by lichen on 9/27/17.
+ */
+class PdfMakerSpec extends spock.lang.Specification {
+
+    def 'rtf'
+}
