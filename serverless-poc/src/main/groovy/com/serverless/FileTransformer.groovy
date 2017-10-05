4e1683a5502c6af3e80baae74927fc054d6a5d66
Lihe Chen <lihe.chen@changehealthcare.com>
2017-10-05 14:37:34 -0500

    Works?

--- serverless-poc/src/main/groovy/com/serverless/FileTransformer.groovy
+++ serverless-poc/src/main/groovy/com/serverless/FileTransformer.groovy
@@ -2,24 +2,33 @@ package com.serverless
 
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
-import com.amazonaws.services.s3.model.S3Event
+import com.amazonaws.services.s3.event.S3EventNotification
+import com.amazonaws.services.lambda.runtime.events.S3Event;
 import groovy.transform.CompileStatic
 import org.apache.log4j.Logger
+import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord
 
 /**
  * Created by lichen on 10/3/17.
  */
 @CompileStatic
-class FileTransformer implements RequestHandler<S3Event, Integer> {
+class FileTransformer implements RequestHandler<S3Event, String> {
 
     private static final Logger LOG = Logger.getLogger(FileTransformer.class)
 
     @Override
-    Integer handleRequest(S3Event event, Context context) {
-        Map<String, Object> values = event.getProperties()
-        values.keySet().each {
-            key -> LOG.info("${key}:")
+    String handleRequest(S3Event event, Context context) {
+        try {
+            LOG.info("file created ! ...")
+            S3EventNotificationRecord record = event.records.first()
+
+            LOG.info("awsRegion: ${record.awsRegion}")
+            LOG.info("eventName: ${record.eventName}")
+            LOG.info("eventSource: ${record.eventSource}")
+            LOG.info(record.s3.bucket.name)
+            return "OK"
+        }catch (IOException e) {
+            return e.message;
         }
-        return 0;
     }
 }
