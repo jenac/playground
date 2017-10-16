8ff2f2037519d0f9934528074a9d5b986ad6ed80
Lihe Chen <lihe.chen@changehealthcare.com>
2017-10-16 13:44:37 -0500

    added empty project

---
+++ spark-poc/src/main/groovy/com/chc/poc/sparkpoc/SparkPocApplication.groovy
@@ -0,0 +1,12 @@
+package com.chc.poc.sparkpoc
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SparkPocApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run SparkPocApplication, args
+	}
+}
