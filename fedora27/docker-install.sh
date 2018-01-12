f42b7df5b3167fb09bf450d4826e451c5f8b944f
Lihe Chen <lihe.chen@gmail.com>
2018-01-12 12:26:54 -0600

    added docker;

---
+++ fedora27/docker-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo dnf remove docker \
+                  docker-common \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install docker-ce
+sudo systemctl start docker
+
