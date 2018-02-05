03c678f9a1e0613cc3f234b2f2ce3c5e573506ba
Chen, Lihe <lihe.chen@changehealthcare.com>
2018-02-05 13:33:00 -0600

    fix error

--- nestjs-poc/src/logger.spec.ts
+++ nestjs-poc/src/logger.spec.ts
@@ -1,13 +1,11 @@
 import { logger } from './logger';
 import { expect } from 'chai';
 import 'mocha';
-import { unlink, unlinkSync } from 'fs';
 
 describe('Hello function', () => {
   it('should return hello world', (done) => {
       
-      unlinkSync("./*.log");
-    // logger.instance.error('haha');
+    logger.instance.error('haha');
     done();
   });
 
