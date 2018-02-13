f443a5fa71827bc293744c3c7c7eea5150e04cef
Chen, Lihe <lihe.chen@changehealthcare.com>
2018-02-13 14:34:24 -0600

    wip

--- nestjs-poc/src/api/account.controller.ts
+++ nestjs-poc/src/api/account.controller.ts
@@ -1,11 +1,50 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Post, Body, HttpCode, Param } from "@nestjs/common";
+import { UserDTO } from "./models/user.dto";
+import { ManagedUserVM } from "./models/manage.user.vm";
+import { HTTP_CREATED } from "./api.constants";
+import { KeyAndPasswordVM } from "./models/key.password.vm";
 
 @Controller("api")
 export class AccountController {
 
-    //get self with token?
-    @Get("account")
-    async getAccount() {
-      return { message: "hello, world" }
-    }
+  @Post("register")
+  @HttpCode(HTTP_CREATED)
+  async registerAccount( @Body() managedUserVM: ManagedUserVM): Promise<void> {
+
+  }
+
+  @Get("activate")
+  async activateAccount( @Param("key") key: String): Promise<void> {
+  }
+
+  @Get("/authenticate")
+  async isAuthenticated(): Promise<string> {
+    return 'admin'
+  }
+
+  @Get("/account")
+  async getAccount(): Promise<UserDTO> {
+    return null
+  }
+
+  @Post("/account")
+  async saveAccount( @Body() userDTO: UserDTO): Promise<void> {
+
+  }
+
+  @Post("account/change-password")
+  async changePassword( @Body() password: string): Promise<void> {
+
+  }
+
+  @Post("account/reset-password/init")
+  async requestPasswordReset( @Body() mail: string): Promise<void> {
+
+  }
+
+  @Post("account/reset-password/finish")
+  async finishPasswordReset( @Body() keyAndPassword: KeyAndPasswordVM): Promise<void> {
+
+  }
 }
+
