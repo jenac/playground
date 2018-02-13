83fb790cbb8456ae4fb4326ef78f93caa8058675
Chen, Lihe <lihe.chen@changehealthcare.com>
2018-02-12 18:14:59 -0600

    wip

--- nestjs-poc/src/app.controller.ts
+++ nestjs-poc/src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Get, Controller, Logger as Logger2 } from '@nestjs/common';
 
-import { Logger} from './shared/logger';
+import { Logger } from './shared/logger';
+import * as qrccode from 'qrcode';
 
 @Controller()
 export class AppController {
   constructor(private readonly logger: Logger) {}
 	@Get()
 	root(): string {
+    qrccode.toFile('./qr.jpg', 'adasfasd');
     let s = new Logger2("kkk", true);
     s.log('waht?');
     this.logger.instance.info("hello, world");
