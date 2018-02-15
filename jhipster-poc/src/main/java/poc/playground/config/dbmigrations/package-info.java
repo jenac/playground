c575f928dbe5e55ac8727d3cc0c4fdee4569b71e
Lihe Chen <lihe.chen@gmail.com>
2018-02-15 12:32:31 -0600

    added jhipster poc

---
+++ jhipster-poc/src/main/java/poc/playground/config/dbmigrations/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * MongoDB database migrations using MongoBee.
+ */
+package poc.playground.config.dbmigrations;
---
+++ jhipster-poc/src/main/java/poc/playground/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package poc.playground.service.dto;
