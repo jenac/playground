b011a60ae4f7bdbcc195fdad22d530efb30ae509
Lihe Chen <lihe.chen@gmail.com>
2018-02-21 15:44:40 -0600

    mf

--- jhipster-poc/src/main/webapp/app/admin/bot/bot.component.ts
+++ jhipster-poc/src/main/webapp/app/admin/bot/bot.component.ts
@@ -1,45 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 import { BotService } from './bot.service';
+import { BotState } from './bot.model';
 
 @Component({
     selector: 'jhi-bot',
     templateUrl: './bot.component.html'
 })
 export class BotComponent implements OnInit {
-    allBot: any = null;
-    bot: any = null;
-    configKeys: any[];
-    filter: string;
-    orderProp: string;
-    reverse: boolean;
-
-    constructor(
-        private botService: BotService
-    ) {
-        this.configKeys = [];
-        this.filter = '';
-        this.orderProp = 'prefix';
-        this.reverse = false;
-    }
-
-    keys(dict): Array<string> {
-        return (dict === undefined) ? [] : Object.keys(dict);
-    }
-
+    botState: BotState;
+    constructor(private botService: BotService) { }
     ngOnInit() {
-        this.botService.get().subscribe((bot) => {
-            this.bot = bot;
-
-            for (const config of bot) {
-                if (config.properties !== undefined) {
-                    this.configKeys.push(Object.keys(config.properties));
-                }
-            }
-        });
-
-        this.botService.getEnv().subscribe((bot) => {
-            this.allBot = bot;
-        });
+        // this.botService.getEnv().subscribe((bot) => {
+        //     this.allBot = bot;
+        // });
+        this.botState = {
+            loginUrl: 'https://finance.google.com',
+            state: 'login'
+        };
     }
 }
