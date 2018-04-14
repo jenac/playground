a0c0aaffaba571706e523450bb4c341d900ce9e8
Lihe Chen <lihe.chen@gmail.com>
2018-04-13 21:07:23 -0500

    haha

---
+++ ubuntu1604/elementary-tweaks.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:philip.scott/elementary-tweaks -y
+sudo apt-get update
+sudo apt-get install elementary-tweaks -y
--- ubuntu1604/git-config.sh
+++ ubuntu1604/git-config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git config --global user.name "Lihe Chen"
 git config --global user.email "lihe.chen@gmail.com"
-git config --global credential.helper cache
-git config --global credential.helper "cache --timeout=7200"
+git config --global credential.helper store
+
