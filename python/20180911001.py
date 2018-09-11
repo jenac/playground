740ba3a135ff5839dd2196f07e583dfb47ebde9a
Chen, Lihe <lihe.chen@changehealthcare.com>
2018-09-11 08:25:58 -0500

    wip

--- python/20180911001.py
+++ python/20180911001.py
@@ -1,6 +1,7 @@
-from mpl_finance import quotes_historical_yahoo
-ticker='DELL'
-begdate=(2013,1,1)
-enddate=(2013,11,9)
-p=quotes_historical_yahoo(ticker, begdate, enddate,asobject=True, adjusted=True)
-print(p)
+import pandas_datareader.data as web
+import datetime
+start = datetime.datetime(2010, 1, 1)
+end = datetime.datetime(2013, 1, 27)
+f = web.DataReader('F', 'google', start, end)
+print(f.ix['2010-01-04'])
+print(f)
