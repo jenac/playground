59d2c2891c040d416a444323170067a9cc807f46
Lihe Chen <lihe.chen@gmail.com>
2018-11-16 09:26:04 -0600

    added

---
+++ cmder-alias/oh-myzsh.bat
@@ -0,0 +1,8 @@
+alias gst=git status  
+alias gco=git checkout $1  
+alias gba=git branch -a  
+alias gcm=git checkout master  
+alias gl=git pull  
+alias ggp=git push  
+alias gcb=git checkout -b $1  
+alias gfa=git fetch --all --prune  
