ffb121cbfadb839de24e04a947fda1a9cd770c53
Chen, Lihe <lihe.chen@changehealthcare.com>
2019-01-02 08:57:23 -0600

    wip

--- ideas-app-home/ideas-api/src/user/user.service.ts
+++ ideas-app-home/ideas-api/src/user/user.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from "typeorm";
 
 @Injectable()
 export class UserService {
+  constructor(@InjectRepository() userRepository: Repository<UserEntity>)
 
 }
