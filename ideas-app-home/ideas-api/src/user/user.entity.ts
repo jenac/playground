c80a0fd21ec443e2d6dd2f2723328dea9dafda6c
Chen, Lihe <lihe.chen@changehealthcare.com>
2019-01-02 10:50:34 -0600

    added pg docker and typeorm

---
+++ ideas-app-home/ideas-api/src/user/user.entity.ts
@@ -0,0 +1,21 @@
+import { Entity, PrimaryGeneratedColumn, CreateDateColumn, Column, BeforeInsert } from "typeorm";
+import * as bcrypt from 'bcryptjs';
+@Entity('user')
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @CreateDateColumn()
+  created: Date;
+
+  @Column({type: 'text', unique: true})
+  username: string;
+
+  @Column('text')
+  password: string;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 11); //11 should be in const
+  }
+}
--- ideas-app-home/ideas-api/src/user/user.service.ts
+++ ideas-app-home/ideas-api/src/user/user.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from "typeorm";
+import { UserEntity } from "./user.entity";
 
 @Injectable()
 export class UserService {
-  constructor(@InjectRepository() userRepository: Repository<UserEntity>)
+  constructor(@InjectRepository(UserEntity) userRepository: Repository<UserEntity>) {}
 
+  
 }
