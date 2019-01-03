e391bf49a0891c1d045b0d7f960ce0dc570dffdb
Chen, Lihe <lihe.chen@changehealthcare.com>
2019-01-03 08:47:00 -0600

    added authguard

--- ideas-app-home/ideas-api/src/auth/auth.controller.ts
+++ ideas-app-home/ideas-api/src/auth/auth.controller.ts
@@ -1,22 +1,29 @@
-import { Controller, Post, Body, UsePipes } from "@nestjs/common";
+import { Controller, Post, Body, UsePipes, Get, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { async } from "rxjs/internal/scheduler/async";
 import { CredDTO } from "./cred.dto";
 import { ValidationPipe } from "src/shared/validation.pipe";
+import { AuthGuard } from "src/shared/auth.guard";
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('register')
+  @Post('auth/register')
   @UsePipes(ValidationPipe)
   async register(@Body() cred: CredDTO) {
     return await this.authService.register(cred);
   }
 
-  @Post('login')
+  @Post('auth/login')
   @UsePipes(ValidationPipe)
   async login(@Body() cred: CredDTO) {
     return await this.authService.login(cred);
+  }
+
+  @Get('auth/whoami')
+  @UseGuards(AuthGuard)
+  async me() {
+    return 'not implemented'
   }
 }
---
+++ ideas-app-home/ideas-api/src/shared/auth.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable, CanActivate, ExecutionContext, HttpException, HttpStatus } from "@nestjs/common";
+import * as jwt from "jsonwebtoken";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    if (request) {
+      if (!request.headers.authorization) {
+        return false;
+      }
+      request.user = await this.validateToken(request.headers.authorization);
+      return true;
+    }
+  }
+
+  async validateToken(auth: string) {
+    const splitted = auth.split(' ');
+    if (splitted[0] !== 'Bearer') {
+      throw new HttpException('Invalid token', HttpStatus.UNAUTHORIZED);
+    }
+    const token = splitted[1];
+    try {
+      const decoded: any = await jwt.verify(token, process.env.SECRET);
+      return decoded;
+    } catch (err) {
+      const message = `Token error: ${err.message || err.name}`;
+      throw new HttpException(message, HttpStatus.UNAUTHORIZED);
+    }
+  }
+}
