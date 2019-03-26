21816c8dd5b6f6a9977d7407b7bd67fe91fe5ceb
Lihe Chen <lihe.chen@rallyhealth.com>
2019-03-26 15:01:38 -0500

    OK

--- python/yahoo.py
+++ python/yahoo.py
@@ -6,6 +6,9 @@ r.text[438696:438796] # returns 'CrumbStore":{"crumb":"blusAuTOM1e"},"StreamStor
 c = s.get("https://query1.finance.yahoo.com/v7/finance/download/aapl?period1=1491004799&period2=1553616324&interval=1d&events=history&crumb=blusAuTOM1e")
 c.text
 
-from datetime import date, timedelta
-ten = date.today() - timedelta(days=3650)
-ten
+from datetime import datetime, timedelta
+ten = datetime.now() - timedelta(days=3650)
+ts = int(ten.timestamp())
+ts
+url = "https://google.com?s={}".format(ts)
+url
