2fdf20f5a1cce1dde0a1af5e126678fe54080fb5
Lihe Chen <lihe.chen@rallyhealth.com>
2019-05-22 10:30:24 -0500

    added 404

--- react-poc/src/App.js
+++ react-poc/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AboutPage from './components/about/AboutPage';
 import HomePage from './components/home/HomePage';
 import Header from './components/common/Header';
+import PageNotFound from './components/PageNotFound';
 
 function App() {
   return (
     <Router>
       <Header></Header>
       <div className="container-fluid">
-        <Route exact path="/" component={HomePage}></Route>
-        <Route path="/about" component={AboutPage}></Route>
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route path="/about" component={AboutPage}></Route>
+          <Route component={PageNotFound}></Route>
+        </Switch>
       </div>
     </Router>
   );
