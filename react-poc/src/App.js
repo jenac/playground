5013972d3538f8f65c5c87ac927ba4a74f26d574
Lihe Chen <lihe.chen@rallyhealth.com>
2019-05-22 10:11:23 -0500

    v1

--- react-poc/src/App.js
+++ react-poc/src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AboutPage from './components/about/AboutPage';
+import HomePage from './components/home/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container-fluid">
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/about" component={AboutPage}></Route>
+      </div>
+    </Router>
   );
 }
 
---
+++ react-poc/src/components/about/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <div>
+        <h2>About</h2>
+        <p>
+            OK, this is too long to read.
+        </p>
+    </div>
+);
+
+export default AboutPage;
