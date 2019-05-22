12991efd32a77fec4a876f4723403e1e0fd9ae04
Lihe Chen <lihe.chen@rallyhealth.com>
2019-05-22 10:25:25 -0500

    added header

--- react-poc/src/App.js
+++ react-poc/src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import AboutPage from './components/about/AboutPage';
 import HomePage from './components/home/HomePage';
+import Header from './components/common/Header';
 
 function App() {
   return (
     <Router>
+      <Header></Header>
       <div className="container-fluid">
         <Route exact path="/" component={HomePage}></Route>
         <Route path="/about" component={AboutPage}></Route>
