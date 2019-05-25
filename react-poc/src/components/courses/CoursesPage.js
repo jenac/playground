d7b806e4d0fcc860f78e20bf82a7b46a4fd3d9c7
Lihe Chen <lihe.chen@gmail.com>
2019-05-24 22:03:31 -0500

    load courses

--- react-poc/src/components/courses/CoursesPage.js
+++ react-poc/src/components/courses/CoursesPage.js
@@ -2,40 +2,25 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import * as courseActions from '../../redux/actions/courseActions';
+import { bindActionCreators } from "redux";
 
 class CoursesPage extends React.Component {
-    state = {
-        course: {
-            title: ""
-        }
-    }; //for input only
-
-    handleChange = event => {
-        const course = { ...this.state.course, title: event.target.value };
-        this.setState({ course });
+    componentDidMount() {
+        this.props.loadCourses().catch(error => {
+            console.error("Loading courses failed", error);
+        })
     }
-
-    handleSubmit = event => {
-        event.preventDefault();
-        this.props.createCourse(this.state.course);
-        console.log(this.state.course);
-    }
-
+    
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
+            <>
                 <h2>Course</h2>
-                <h3>Add Course</h3>
-                <input type="text"
-                    onChange={this.handleChange}
-                    value={this.state.course.title}>
-                </input>
-                <input type="submit" value="Save">
-                </input>
+                <ul>
                 {this.props.courses.map(course => (
-                    <div key={course.title}> {course.title}</div>
+                    <li key={course.title}> {course.title}</li>
                 ))}
-            </form>
+                </ul>
+            </>
         );
     }
 }
@@ -53,7 +38,7 @@ function mapStateToProps(state) { //state from reducer, an array
 }
 
 const mapDispatchToProps = {
-    createCourse: courseActions.createCourse
+    loadCourses: courseActions.loadCourses
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage); 
--- react-poc/src/redux/actions/courseActions.js
+++ react-poc/src/redux/actions/courseActions.js
@@ -1,4 +1,23 @@
 import * as types from './actionTypes';
+import * as courseApi from '../../api/courseApi';
+
 export function createCourse(course) {
     return { type: types.CREATE_COURSE, course };
+}
+
+export function loadCoursesSuccess(courses) {
+    return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function loadCourses() {
+    return function(dispacth) {
+        return courseApi
+            .getCourses()
+            .then(courses => {
+                dispacth(loadCoursesSuccess(courses));
+            })
+            .catch(error => {
+                throw error;
+            });
+    };
 }
