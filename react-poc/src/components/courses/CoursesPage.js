fa18467d6e511c2f07b1cfc751ac9df388261526
Lihe Chen <lihe.chen@gmail.com>
2019-05-25 13:57:10 -0500

    weip

--- react-poc/src/components/courses/CoursesPage.js
+++ react-poc/src/components/courses/CoursesPage.js
@@ -9,14 +9,15 @@ import { bindActionCreators } from 'redux';
 
 class CoursesPage extends React.Component {
     componentDidMount() {
-        if (this.props.courses.length === 0) {
-            this.props.actions.loadCourses().catch(error => {
+        let { actions, courses, authors } = this.props;
+        if (courses.length === 0) {
+            actions.loadCourses().catch(error => {
                 console.error("Loading courses failed", error);
             });     
         }
         
-        if (this.props.authors.length === 0) {
-            this.props.actions.loadAuthors().catch(error => {
+        if (authors.length === 0) {
+            actions.loadAuthors().catch(error => {
                 console.error("Loading authors failed", error);
             });
         }
@@ -53,14 +54,6 @@ function mapStateToProps(state) { //state from reducer, an array
         authors: state.authors
     };
 }
-
-// const mapDispatchToProps = {
-//     actions: {
-//         loadCourses: courseActions.loadCourses,
-//         locdAuthors: authorActions.loadAuthors
-//     }
-    
-// }
 
 function mapDispatchToProps(dispatch) {
     return {
