bbb0d40691ee56372f7ed8080580e5f0b0690bdf
Lihe Chen <lihe.chen@gmail.com>
2019-10-13 14:34:27 -0500

    wip

--- react-native-examples/AwesomeProject/App.js
+++ react-native-examples/AwesomeProject/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Greeting from './Greeting';
+import Blink from './Blink';
+import LotsOfStyles from './LotsOfStyles';
+import FixedDimensionsBasics from './FixedDimensionsBasics';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Greeting name="Jenac"></Greeting>
+      <Greeting name="DosBox"></Greeting>
+      <Greeting name="MAME"></Greeting>
+      <Blink text= 'I love blink'></Blink>
+      <LotsOfStyles></LotsOfStyles>
+      <FixedDimensionsBasics></FixedDimensionsBasics>
     </View>
   );
 }
