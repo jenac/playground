e53393743384537de40a95f204e3dfd26f2835fa
Lihe Chen <lihe.chen@gmail.com>
2019-10-13 15:32:26 -0500

    wip

--- react-native-examples/AwesomeProject/App.js
+++ react-native-examples/AwesomeProject/App.js
@@ -4,16 +4,34 @@ import Greeting from './Greeting';
 import Blink from './Blink';
 import LotsOfStyles from './LotsOfStyles';
 import FixedDimensionsBasics from './FixedDimensionsBasics';
+import FlexDimensionsBasics from './FlexDimensionsBasics';
+import FlexDirectionBasics from './FlexDirectionBasics';
+import JustifyContentBasics from './JustifyContentBasics';
+import AlignItemsBasics from './AlignItemsBasics';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Greeting name="Jenac"></Greeting>
-      <Greeting name="DosBox"></Greeting>
-      <Greeting name="MAME"></Greeting>
-      <Blink text= 'I love blink'></Blink>
-      <LotsOfStyles></LotsOfStyles>
-      <FixedDimensionsBasics></FixedDimensionsBasics>
-    </View>
+    // 1.
+    // <View style={styles.container}>
+    //   <Greeting name="Jenac"></Greeting>
+    //   <Greeting name="DosBox"></Greeting>
+    //   <Greeting name="MAME"></Greeting>
+    //   <Blink text= 'I love blink'></Blink>
+    //   <LotsOfStyles></LotsOfStyles>
+    //   <FixedDimensionsBasics></FixedDimensionsBasics>
+    // </View>
+
+    // 2.
+    // <FlexDimensionsBasics></FlexDimensionsBasics>
+
+    // 3.
+    // <FlexDirectionBasics></FlexDirectionBasics>
+
+    // 4.
+    // <JustifyContentBasics></JustifyContentBasics>
+
+    // 5.
+    <AlignItemsBasics></AlignItemsBasics>
   );
 }
 
