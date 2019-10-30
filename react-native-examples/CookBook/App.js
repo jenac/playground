88b86fc29e8a1f780ee727e7f0b9783b39b0ca19
Lihe Chen <lihe.chen@gmail.com>
2019-10-29 20:33:43 -0500

    ch02

--- react-native-examples/CookBook/App.js
+++ react-native-examples/CookBook/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ch0202 } from './components/Ch0202';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Ch0202></Ch0202>
+      <Text>Open up App.js to start working on your app! What?</Text>
     </View>
   );
 }
---
+++ react-native-examples/CookBook/components/Ch0202.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+export class Ch0202 extends Component {
+    render() {
+        return (
+            <Text>Hello world!</Text>
+        );
+    }
+}
