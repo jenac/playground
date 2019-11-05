9c2ec412a893c836269505173ddbcb4ad67561f8
Lihe Chen <lihe.chen@rallyhealth.com>
2019-11-05 09:00:16 -0600

    re create project using mssql local db

--- csharp_whatsnew/spa_react/Startup.cs
+++ csharp_whatsnew/spa_react/Startup.cs
@@ -27,7 +27,7 @@ namespace spa_react
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
+                options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddDefaultIdentity<ApplicationUser>()
