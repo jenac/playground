e37b362d6fbb52620440fc99d5c0f56acb56e00d
Lihe Chen <lihe.chen@rallyhealth.com>
2019-11-05 12:40:59 -0600

    added manual sql scripts

---
+++ csharp_whatsnew/spa_react/Manual_Sql_Scripts/seed_data.sql
@@ -0,0 +1,15 @@
+insert into tblSkills (Title) values('Visual Foxpro');
+insert into tblSkills (Title) values('C#');
+insert into tblSkills (Title) values('VB.NET');
+insert into tblSkills (Title) values('Delphi');
+insert into tblSkills (Title) values('Java');
+insert into tblSkills (Title) values('Power Builder');
+insert into tblSkills (Title) values('COBOL');
+insert into tblSkills (Title) values('Python');
+
+Insert Into tblEmployees (EmployeeName,PhoneNumber,SkillID,YearsExperience) values ('Chen 1','9869569634',2,'11');
+Insert Into tblEmployees (EmployeeName,PhoneNumber,SkillID,YearsExperience) values ('Li 2','9869166077',8,'14');
+Insert Into tblEmployees (EmployeeName,PhoneNumber,SkillID,YearsExperience) values ('Zhang 3','9869569634',1,'24');
+Insert Into tblEmployees (EmployeeName,PhoneNumber,SkillID,YearsExperience) values ('Zheng 4','9969359746',6,'20');
+Insert Into tblEmployees (EmployeeName,PhoneNumber,SkillID,YearsExperience) values ('Wang 5','9869166012',7,'28');
+Insert Into tblEmployees (EmployeeName,PhoneNumber,SkillID,YearsExperience) values ('Ma 6','9261166012',5,'18');
