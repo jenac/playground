091536db333a16854e9cc0edd35675f703115b12
Lihe Chen <lihe.chen@gmail.com>
2019-11-17 22:10:08 -0600

    components 02 make base class

---
+++ csharp_whatsnew/BlazorTutorial/Shared/ChildComponentBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorTutorial.Shared
+{
+    public class ChildComponentBase : ComponentBase
+    {
+
+        protected bool DarkThemeOn;
+        protected string AlertTheme => DarkThemeOn ? "dark" : "light";
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected override void OnInitialized()
+        {
+            DarkThemeOn = true;
+        }
+    }
+
+
+}
