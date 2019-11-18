4c4fdb3bf82519ea8efd13671776c5508b3e509b
Lihe Chen <lihe.chen@rallyhealth.com>
2019-11-18 14:51:23 -0600

    DI - inject into child component use property inject

--- csharp_whatsnew/BlazorTutorial/Shared/ChildComponentBase.cs
+++ csharp_whatsnew/BlazorTutorial/Shared/ChildComponentBase.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using BlazorTutorial.Services;
 
 namespace BlazorTutorial.Shared
 {
     public class ChildComponentBase : ComponentBase
     {
 
+        [Inject]
+        protected RandomService RandomService { get; set; }
         protected bool DarkThemeOn;
         protected string AlertTheme => DarkThemeOn ? "dark" : "light";
 
