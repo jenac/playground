f4c78119c63a0e0d6ce1b7c5c2faeae19d0eeb8d
Lihe Chen <lihe.chen@gmail.com>
2019-11-19 22:29:14 -0600

    JavaScript interop

---
+++ csharp_whatsnew/BlazorTutorial/wwwroot/js/interop.js
@@ -0,0 +1,18 @@
+﻿
+function createAlert() {
+    alert('Alert from js');
+};
+
+let askQuestion = (question) => {
+    return prompt(question);
+};
+
+window.askQuestion = askQuestion;
+
+function setElementTextById(id, text) {
+    document.getElementById(id).innerText = text;
+}
+
+function focusElement(element) {
+    element.focus();
+}
