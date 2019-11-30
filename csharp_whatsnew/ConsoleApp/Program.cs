06504e28a220feb4d6d8d49d11364b0164563311
Lihe Chen <lihe.chen@gmail.com>
2019-11-30 15:07:09 -0600

    wip

--- csharp_whatsnew/ConsoleApp/Program.cs
+++ csharp_whatsnew/ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 
 namespace ConsoleApp
@@ -7,8 +8,13 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Enumerable.Range(102, 999).Select(i => $"")
+            var inserts = Enumerable.Range(102, 999)
+            .Select(i => $"INSERT INTO Orders (UserName, OrderDate) VALUES ('user{i}@example.com', '{DateTime.Now.AddDays(i-1000).ToString("yyyy-MM-dd")}' );\r\n");
+            File.WriteAllLines("inserts.sql", inserts);
+
             Console.WriteLine("Hello World!");
         }
+
+        
     }
 }
