79ab79f657e8b92b4b053756db1d68175559cc06
Lihe Chen <lihe.chen@gmail.com>
2019-11-30 10:15:03 -0600

    added a console

---
+++ csharp_whatsnew/ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enumerable.Range(102, 999).Select(i => $"")
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
