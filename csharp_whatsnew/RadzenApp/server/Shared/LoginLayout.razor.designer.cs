3954e032a7a7462d223ff32339facf4899e70860
Lihe Chen <lihe.chen@gmail.com>
2019-12-03 21:34:45 -0600

    init radzen app

---
+++ csharp_whatsnew/RadzenApp/server/Shared/LoginLayout.razor.designer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Radzen;
+using Radzen.Blazor;
+namespace RadzenApp.Layouts
+{
+    public partial class LoginLayoutComponent : LayoutComponentBase
+    {
+        [Inject]
+        protected NavigationManager UriHelper { get; set; }
+
+        [Inject]
+        protected DialogService DialogService { get; set; }
+
+        [Inject]
+        protected NotificationService NotificationService { get; set; }
+
+        protected RadzenBody body0;
+
+        protected override async System.Threading.Tasks.Task OnInitializedAsync()
+        {
+        }
+
+    }
+}
---
+++ csharp_whatsnew/RadzenApp/server/Shared/MainLayout.razor.designer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Radzen;
+using Radzen.Blazor;
+namespace RadzenApp.Layouts
+{
+    public partial class MainLayoutComponent : LayoutComponentBase
+    {
+        [Inject]
+        protected NavigationManager UriHelper { get; set; }
+
+        [Inject]
+        protected DialogService DialogService { get; set; }
+
+        [Inject]
+        protected NotificationService NotificationService { get; set; }
+
+        protected RadzenBody body0;
+
+        protected RadzenSidebar sidebar0;
+
+        protected override async System.Threading.Tasks.Task OnInitializedAsync()
+        {
+        }
+
+
+        protected async void SidebarToggle0Click(dynamic args)
+        {
+            sidebar0.Toggle();
+
+            body0.Toggle();
+        }
+    }
+}
