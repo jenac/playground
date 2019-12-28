e522c3a94a46acb78d5c3b3bf9453feca7555f2a
L Chen <jenac@users.noreply.github.com>
2019-12-27 19:46:16 -0600

    Update ohmyzsh-install.sh

--- ubuntu1804/ohmyzsh-install.sh
+++ ubuntu1804/ohmyzsh-install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo apt install zsh -y
+sudo apt-get install powerline fonts-powerline
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
