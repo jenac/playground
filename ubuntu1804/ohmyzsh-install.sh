ca2a3acb36f1e4e53d08f50a7449ece4d68a2f64
Lihe Chen <lihe.chen@gmail.com>
2019-12-27 19:56:24 -0600

    change theme

--- ubuntu1804/ohmyzsh-install.sh
+++ ubuntu1804/ohmyzsh-install.sh
@@ -2,3 +2,7 @@
 sudo apt install zsh -y
 sudo apt-get install powerline fonts-powerline
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#nano ~/.zshrc
+#ZSH_THEME="agnoster"
+#Change terminal color to Solarized(dark)
