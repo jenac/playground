d2df6ee25b825b16b2810312d858fb7c5cc9d4fd
Lihe Chen <lihe.chen@gmail.com>
2020-01-25 13:31:29 -0600

    wip

---
+++ angular/rxjs-chat/src/app/user/users.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Subject, BehaviorSubject } from 'rxjs';
+import { User } from './user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  // constructor() { }
+
+  currentUser: Subject<User> = new BehaviorSubject<User>(null);
+
+  public setCurrentUser(newUser: User): void {
+    this.currentUser.next(newUser);
+  }
+}
+
+export const userServiceInjectables: Array<any> = [
+  UsersService
+];
