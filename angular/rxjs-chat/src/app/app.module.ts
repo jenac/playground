f8928e0a8dc660dcf2c6b42a01a5ff577982ee06
Lihe Chen <lihe.chen@gmail.com>
2020-01-25 14:04:49 -0600

    wip

--- angular/rxjs-chat/src/app/app.module.ts
+++ angular/rxjs-chat/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ChatWindowComponent } from './chat-window/chat-window.component';
+import { ChatThreadsComponent } from './chat-threads/chat-threads.component';
+import { ChatThreadComponent } from './chat-thread/chat-thread.component';
+import { ChatPageComponent } from './chat-page/chat-page.component';
+import { ChatNavBarComponent } from './chat-nav-bar/chat-nav-bar.component';
+import { ChatMessageComponent } from './chat-message/chat-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatWindowComponent,
+    ChatThreadsComponent,
+    ChatThreadComponent,
+    ChatPageComponent,
+    ChatNavBarComponent,
+    ChatMessageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular/rxjs-chat/src/app/chat-window/chat-window.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit, ElementRef, ChangeDetectionStrategy } from '@angular/core';
+import { Message } from '../message/message.model';
+import { Observable } from 'rxjs';
+import { Thread } from '../thread/thread.model';
+import { User } from '../user/user.model';
+import { MessagesService } from '../message/messages.service';
+import { ThreadsService } from '../thread/threads.service';
+import { UsersService } from '../user/users.service';
+
+@Component({
+  selector: 'chat-window',
+  templateUrl: './chat-window.component.html',
+  styleUrls: ['./chat-window.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ChatWindowComponent implements OnInit {
+  messages: Observable<any>;
+  currentThread: Thread;
+  draftMessage: Message;
+  currentUser: User;
+
+  constructor(public messagesService: MessagesService,
+              public threadsService: ThreadsService,
+              public UsersService: UsersService,
+              public el: ElementRef) {
+  }
+
+  ngOnInit(): void {
+    this.messages = this.threadsService.currentThreadMessages;
+
+    this.draftMessage = new Message();
+
+    this.threadsService.currentThread.subscribe(
+      (thread: Thread) => {
+        this.currentThread = thread;
+      });
+
+    this.UsersService.currentUser
+      .subscribe(
+        (user: User) => {
+          this.currentUser = user;
+        });
+
+    this.messages
+      .subscribe(
+        (messages: Array<Message>) => {
+          setTimeout(() => {
+            this.scrollToBottom();
+          });
+        });
+  }
+
+  onEnter(event: any): void {
+    this.sendMessage();
+    event.preventDefault();
+  }
+
+  sendMessage(): void {
+    const m: Message = this.draftMessage;
+    m.author = this.currentUser;
+    m.thread = this.currentThread;
+    m.isRead = true;
+    this.messagesService.addMessage(m);
+    this.draftMessage = new Message();
+  }
+
+  scrollToBottom(): void {
+    const scrollPane: any = this.el
+      .nativeElement.querySelector('.msg-container-base');
+    scrollPane.scrollTop = scrollPane.scrollHeight;
+  }
+}
