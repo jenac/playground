6aff6c1b50ee92e5ac695e50f52c236b64b92180
Lihe Chen <lihe.chen@gmail.com>
2020-02-02 10:43:07 -0600

    wip

--- angular/advanced-components/src/app/app.module.ts
+++ angular/advanced-components/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { StyleEncapsulationComponent } from './style-encapsulation/style-encapsulation.component';
 import { NativeEncapsulationComponent } from './native-encapsulation/native-encapsulation.component';
+import { PopupDirective } from './popup.directive';
+import { PopupDemoComponent } from './popup-demo/popup-demo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StyleEncapsulationComponent,
-    NativeEncapsulationComponent
+    NativeEncapsulationComponent,
+    PopupDirective,
+    PopupDemoComponent
   ],
   imports: [
     BrowserModule
