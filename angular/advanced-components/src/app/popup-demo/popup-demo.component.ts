46fe0d895237e82b98c31076235939f1970b64d9
Lihe Chen <lihe.chen@gmail.com>
2020-02-02 10:50:35 -0600

    wip

--- angular/advanced-components/src/app/popup-demo/popup-demo.component.ts
+++ angular/advanced-components/src/app/popup-demo/popup-demo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-popup-demo',
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PopupDemoComponent implements OnInit {
 
-  constructor() { }
+  constructor(_elementRef: ElementRef) {
+    console.log(_elementRef);
+  }
 
   ngOnInit() {
   }
