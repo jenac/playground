426ea020a0e26b60a918b9403c86cba9d8e49360
Lihe Chen <lihe.chen@rallyhealth.com>
2020-02-05 13:49:48 -0600

    wip

--- scala-cookbook/ch01/src/test/scala/example/HelloSpec.scala
+++ scala-cookbook/ch01/src/test/scala/example/HelloSpec.scala
@@ -6,7 +6,7 @@ class HelloSpec extends FlatSpec with Matchers {
     Hello.greeting shouldEqual "hello"
   }
 
-  "1.10.a" should "work" in {
+  it should "1.10" in {
     import example.utils.StringUtils._
     val result = "HAL".increment
     result shouldEqual "IBM"
@@ -14,5 +14,28 @@ class HelloSpec extends FlatSpec with Matchers {
     "4".plusOne shouldBe 5
     "0".asBoolean shouldBe false
     "1".asBoolean shouldBe true
+  }
+
+  it should "2.01" in {
+    "100".toInt shouldBe 100
+    "100".toDouble shouldBe 100.0
+    "100".toFloat shouldBe 100.0
+    "1".toLong shouldBe 1
+    "1".toShort shouldBe 1
+    "1".toByte shouldBe 1
+
+    Integer.parseInt("1", 2) shouldBe 1
+    Integer.parseInt("10", 2) shouldBe 2
+    Integer.parseInt("100", 2) shouldBe 4
+    Integer.parseInt("1", 8) shouldBe 1
+    Integer.parseInt("10", 8) shouldBe 8
+    Integer.parseInt("11", 8) shouldBe 9
+
+    import example.utils.ValueUtils._
+    "1".toInt(2) shouldBe 1
+    "10".toInt(2) shouldBe 2
+    "100".toInt(2) shouldBe 4
+    "100".toInt(8) shouldBe 64
+    "100".toInt(16) shouldBe 256
   }
 }
