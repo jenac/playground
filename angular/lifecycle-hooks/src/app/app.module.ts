92bdfb8e8e338357078c04f1e22267d3001c717d
Lihe Chen <lihe.chen@gmail.com>
2020-02-04 20:26:09 -0600

    wip

--- angular/lifecycle-hooks/src/app/app.module.ts
+++ angular/lifecycle-hooks/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OnInitComponent } from './on-init/on-init.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OnInitComponent
   ],
   imports: [
     BrowserModule
