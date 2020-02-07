ea9789537ca8028139feaac2ef1baaaae7907785
Lihe Chen <lihe.chen@gmail.com>
2020-02-06 20:10:38 -0600

    to 3.08

--- scala-cookbook/ch01/src/test/scala/example/Ch03Spec.scala
+++ scala-cookbook/ch01/src/test/scala/example/Ch03Spec.scala
@@ -2,19 +2,21 @@ package example
 
 import org.scalatest._
 
+import scala.util.control.Breaks
+
 class Ch03Spec extends FlatSpec with Matchers {
   it should "3.01" in {
     val a = Array("apple", "banana", "orange")
-    for ( e <- a ) {
+    for (e <- a) {
       println(e.toUpperCase)
     }
 
-    val newArray = for ( e <- a ) yield e.toUpperCase
+    val newArray = for (e <- a) yield e.toUpperCase
     newArray(0) shouldBe "APPLE"
     newArray(1) shouldBe "BANANA"
     newArray(2) shouldBe "ORANGE"
 
-    for(i <-0 until a.length) {
+    for (i <- 0 until a.length) {
       a(i).toUpperCase shouldBe newArray(i)
     }
 
@@ -22,16 +24,16 @@ class Ch03Spec extends FlatSpec with Matchers {
       println(s"$index is $e")
     }
 
-    for ( i <- 1 to 10 if i < 4 ) println(i)
+    for (i <- 1 to 10 if i < 4) println(i)
 
     val names = Map("fname" -> "Robert", "lname" -> "Mike")
-    for ((k,v) <- names) println(s"key: $k, value: $v")
+    for ((k, v) <- names) println(s"key: $k, value: $v")
 
     a.foreach(println)
   }
 
   it should "3.02" in {
-    for(i <- 1 to 2; j <- 1 to 2) println(s"i=$i, j=$j")
+    for (i <- 1 to 2; j <- 1 to 2) println(s"i=$i, j=$j")
     println("----------------------------------------")
     for {
       i <- 1 to 3
@@ -46,7 +48,7 @@ class Ch03Spec extends FlatSpec with Matchers {
       i <- 1 to 10
       if i > 3
       if i < 6
-      if i %2 == 0
+      if i % 2 == 0
     } println(i)
   }
 
@@ -57,9 +59,107 @@ class Ch03Spec extends FlatSpec with Matchers {
     capNames.contains("Jenac") shouldBe true
     capNames.contains("Tony") shouldBe true
 
-    val lengths = for(e <- names) yield e.length
+    val lengths = for (e <- names) yield e.length
     lengths.foreach(println)
   }
 
+  it should "3.05" in {
+    import util.control.Breaks._
+    var count = 0
+    breakable {
+      for (i <- 1 to 10) {
+        count += 1
+        if (i > 4) break
+      }
+    }
+    count shouldBe 5
 
+    val searchMe = "peter piper picked a peck of pickled peppers"
+    var numPs = 0
+    for (i <- 0 until searchMe.length) {
+      breakable {
+        if (searchMe.charAt(i) != 'p') {
+          break //only breaks out breakable, still in loop, it simulates continue
+        } else {
+          numPs += 1
+        }
+      }
+    }
+    numPs shouldBe 9
+    searchMe.count(_ == 'p') shouldBe 9
+
+    println("----------nested break----------")
+    val Inner = new Breaks
+    val Outer = new Breaks
+    Outer.breakable {
+      for (i <- 1 to 5) {
+        Inner.breakable {
+          for (j <- 'a' to 'e') {
+            if (i == 1 && j == 'c') Inner.break else println(s"i: $i, j: $j")
+            if (i == 2 && j == 'b') Outer.break
+          }
+        }
+      }
+    }
+
+  }
+
+  it should "factorial" in {
+    import scala.annotation.tailrec
+
+    def factorial(n: Int): Int = {
+      @tailrec def factorialAcc(acc: Int, n: Int): Int = {
+        if (n <= 1) acc
+        else factorialAcc(n * acc, n - 1)
+      }
+
+      factorialAcc(1, n)
+    }
+
+    factorial(3) shouldBe 6
+    factorial(4) shouldBe 24
+    factorial(5) shouldBe 120
+  }
+
+  it should "3.07 using match" in {
+    def toMonth(i: Int) = i match {
+      case 1 => "JAN"
+      case 2 => "FEB"
+      case 3 => "MAR"
+      case 4 => "APR"
+      case 5 => "MAY"
+      case 6 => "JUN"
+      case 7 => "JUL"
+      case 8 => "AUG"
+      case 9 => "SEP"
+      case 10 => "OCT"
+      case 11 => "NOV"
+      case 12 => "DEC"
+      case _ => "Invalid Month"
+    }
+
+    toMonth(1) shouldBe "JAN"
+    toMonth(8) shouldBe "AUG"
+    toMonth(16) shouldBe "Invalid Month"
+
+    //map is better
+    val monthNumberMap = Map(
+      1 -> "JAN",
+      2 -> "FEB",
+      3 -> "MAR",
+      4 -> "APR",
+      5 -> "MAY",
+      6 -> "JUN",
+      7 -> "JUL",
+      8 -> "AUG",
+      9 -> "SEP",
+      10 -> "OCT",
+      11 -> "NOV",
+      12 -> "DEC"
+    )
+
+    monthNumberMap(2) shouldBe "FEB"
+//    monthNumberMap(128) shouldBe "FEB" //throw exception
+    monthNumberMap.get(128) shouldBe None
+  }
 }
