a8ec63b10743cc1bd77b2c1a8146b92294d28619
Lihe Chen <lihe.chen@gmail.com>
2020-02-07 19:54:16 -0600

    ch04

--- scala-cookbook/ch01/src/test/scala/example/Ch04Spec.scala
+++ scala-cookbook/ch01/src/test/scala/example/Ch04Spec.scala
@@ -60,6 +60,75 @@ class Ch04Spec extends FlatSpec with Matchers {
       def getInstance(name: String) = new SomeService(name)
     }
     SomeService.getInstance("whois").hello shouldBe "I am whois service"
+  }
 
+  "4.07" should "private[this] looks more private" in {
+    class Stock {
+      private var price: Double = _
+      def setPrice(p: Double) { price = p }
+      def isHigher(that: Stock) = this.price > that.price //works
+    }
+
+    class MorePrivateStock {
+      private[this] var price: Double = _
+      def setPrice(p: Double) { price = p }
+      //def isHigher(that: MorePrivateStock) = this.price > that.price
+      // fails cause only instance itself can access private[this] member
+      def moreThan100() = this.price > 100.0 //this works!
+    }
+  }
+
+  "4.08" should "lazy is executed when access" in {
+    class Foo {
+      val text = {
+        println("text")
+        "text"
+      }
+      lazy val lazyText = {
+        println("Lazy Text")
+        "lazy"
+      }
+    }
+
+    val foo = new Foo()
+    println("------------------------------")
+    foo.lazyText
+  }
+
+  "4.10" should "sub class constructor"  in {
+    class Person(var name: String, var address: String) {
+      override def toString: String = s"$name @ $address"
+    }
+    class Employee(name: String, address: String, var age: Int) //no var for name, address
+      extends Person(name, address) {
+      override def toString: String = super.toString + s", $age years old"
+    }
+
+    val manager = new Employee("Jenac", "5850 Opus pkwy", 18)
+    manager.toString shouldBe "Jenac @ 5850 Opus pkwy, 18 years old"
+  }
+
+  "4.11" should "call parent class constructor" in {
+    class Animal(var name: String, var age: Int) {
+      def this(name: String) {
+        this(name, 0)
+        println("Animal secondary constructor called")
+      }
+      println("Animal primary constructor called")
+      override def toString: String = s"$name is $age years old"
+    }
+
+    //call parent default constructor
+    class Dog(name: String) extends Animal(name, 0) {
+
+    }
+
+    //call parent secondary constructor
+    class Cat(name: String) extends Animal(name) {
+
+    }
+
+    val dog = new Dog("haha")
+    val cat = new Cat("cccc")
   }
 }
