94bf14cae9cd17dc5c5b415f00cb98431234599e
L Chen <jenac@users.noreply.github.com>
2020-02-14 16:47:49 -0600

    Office (#15)
    merge office

---
+++ scala-cookbook/ch01/src/test/scala/example/Ch06Spec.scala
@@ -0,0 +1,10 @@
+package example
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class Ch06Spec  extends FlatSpec with Matchers {
+  "6.03" should "use getClass" in {
+    val hello = <p>Hello, World</p>
+    hello.child.foreach(e => println(e.getClass))
+  }
+}
