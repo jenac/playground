535a65dbf79fa2f7eb0e7bfc6868028be05a8594
Lihe Chen <lihe.chen@gmail.com>
2020-02-17 14:23:10 -0600

    ch11 wip

--- scala-cookbook/ch01/src/test/scala/example/Ch11Spec.scala
+++ scala-cookbook/ch01/src/test/scala/example/Ch11Spec.scala
@@ -2,6 +2,8 @@ package example
 
 import org.scalatest.{FlatSpec, Matchers}
 
+import scala.collection.mutable.ListBuffer
+
 class Ch11Spec extends FlatSpec with Matchers {
   "11.01" should "create List" in {
     val a = 1 :: 2 :: 3 :: Nil
@@ -27,5 +29,69 @@ class Ch11Spec extends FlatSpec with Matchers {
 
     val g = "too".toList
     g shouldBe List('f', 'o', 'o')
+  }
+
+  "11.02" should "create mutable List" in {
+    val fruits = new ListBuffer[String]()
+    fruits += "Apple"
+    fruits += "Orange"
+    fruits += "Banana"
+    fruits += ("StrawBerry", "Kiwi", "Pineapple")
+
+    fruits -= "Orange"
+    fruits -= ("Apple", "Pineapple")
+
+    val list = fruits.toList
+    list shouldBe List("Banana", "StrawBerry", "Kiwi")
+  }
+
+  "11.03" should "add element to list from head" in {
+    val x = List(2)
+    val y = 1 :: x
+    y shouldBe List(1, 2)
+
+    //will the following slow?
+    val z = 0 +: y
+    z shouldBe List(0, 1, 2)
+
+    val u = x :+ 2
+    u shouldBe List(2, 2)
+  }
+
+  "11.04" should "delete from List/ListBuffer" in {
+    val org = List(5, 1, 4, 3, 2)
+    val a = org.filter(_ > 2)
+    a shouldBe List(5, 4, 3)
+
+    //if need frequent change use ListBuffer
+    val x = ListBuffer(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    x -= 5
+    x -= (2, 3)
+    x shouldBe List(1, 4, 6, 7, 8, 9)
+
+    val e = x.remove(0)
+    e shouldBe 1
+    x shouldBe List(4, 6, 7, 8, 9)
+
+    x.remove(1, 3)
+    x shouldBe ListBuffer(4, 9)
+
+    val xx = ListBuffer(1, 2, 3, 4, 5, 6)
+    xx --= Seq(2, 3, 4)
+    xx.toList shouldBe List(1, 5, 6)
+  }
+
+  "11.05" should "merge list" in {
+    val a = List(1, 2, 3)
+    val b = List(4, 5, 6)
+
+    val c = a ::: b
+    c shouldBe List(1, 2, 3, 4, 5, 6)
+
+    val d = a ++ b
+    d shouldBe List(1, 2, 3, 4, 5, 6)
+
+    val e = List.concat(a, b)
+    e shouldBe List(1, 2, 3, 4, 5, 6)
   }
 }
