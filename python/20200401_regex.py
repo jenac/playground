5ca82a48e47edccf56bd2a330be482d6fff30341
Lihe Chen <lihe.chen@gmail.com>
2020-04-02 07:48:07 -0500

    wip

--- python/20200401_regex.py
+++ python/20200401_regex.py
@@ -145,3 +145,30 @@ print(namesRegex.sub('CENSORED', 'Agent Alice gave secret document to Agent Bob.
 
 agentNamesRegex = re.compile(r'Agent (\w)\w*')
 print(agentNamesRegex.sub(r'\1****', 'Agent Alice told Agent Carol that Agent Eve know Agent Bob was a double agent'))
+
+print('''--------------------
+combine flag''')
+someRegex = re.compile('foo', re.IGNORECASE | re.DOTALL)
+
+print('''--------------------
+real phone number''')
+realPhoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))? # area code
+    (\s|-|\.)? # separator
+    (\d{3}) # first 3 digits
+    (\s|-|\.) # separator
+    (\d{4}) # last 4 digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))? # extension
+    )''', re.VERBOSE)
+print(realPhoneRegex.search('952-123-1111').group())
+print(realPhoneRegex.search('(952)-123-1111').group())
+
+print('''--------------------
+real email''')
+realEmailRegex = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+ # username
+    @ # @ symbol
+    [a-zA-Z0-9.-]+ # domain name
+    (\.[a-zA-Z]{2,4}) # dot-something
+    )''', re.VERBOSE)
+print(realEmailRegex.search("james@s.com").group())
