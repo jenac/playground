3b739034576a0d189e47458cbfbaf82992156208
Lihe Chen <lihe.chen@gmail.com>
2020-04-18 13:47:47 -0500

    wip

--- ubuntu1804/python3.7.sh
+++ ubuntu1804/python3.7.sh
@@ -1,4 +1,8 @@
 sudo apt-get install python3.7
+sudo apt install python3.7-venv
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
 sudo update-alternatives --config python3
+
+# python3.7 -m venv env37
+
