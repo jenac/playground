88933cfc52fc5a33072effe016dd5a6c05824749
Lihe Chen <lihe.chen@gmail.com>
2020-07-01 21:56:20 -0500

    wip
    Signed-off-by: Lihe Chen <lihe.chen@gmail.com>

---
+++ flutter/meditation_app/lib/widgets/category_card.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:meditation_app/constants.dart';
+
+class CategoryCard extends StatelessWidget {
+  final String svgSrc;
+  final String title;
+  final Function press;
+  const CategoryCard({
+    Key key,
+    this.svgSrc,
+    this.title,
+    this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(13),
+      child: Container(
+        // padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(13),
+            boxShadow: [
+              BoxShadow(
+                  offset: Offset(0, 17),
+                  blurRadius: 17,
+                  spreadRadius: -23,
+                  color: kShadowColor),
+            ]),
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: this.press,
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: <Widget>[
+                  Spacer(),
+                  SvgPicture.asset(this.svgSrc),
+                  Spacer(),
+                  Text(
+                    this.title,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline6
+                        .copyWith(fontSize: 15),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
