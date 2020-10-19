90bc69a199ddbcf4063639d5e401d895f37f55cf
Lihe Chen <lihe.chen@rallyhealth.com>
2020-10-19 09:33:43 -0500

    homework
    Signed-off-by: Lihe Chen <lihe.chen@rallyhealth.com>

---
+++ fibo/fibo.js
@@ -0,0 +1,20 @@
+function fibo1(n) {
+    if (n === 0) {
+        return n;
+    }
+
+    let last = 0;
+    let next = 1;
+
+    for (let i = 1; i < n; i++) {
+        let temp = last;
+        last = next;
+        next = last + temp;
+    }
+    return next
+}
+
+
+for (let x = 0; x < 20; x++) {
+    console.log(x, fibo1(x));
+}
